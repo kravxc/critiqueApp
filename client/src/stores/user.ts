f19547d17cf142a46d2cb737e55e778eb95178b6
kravxc <kirill.parfirov@yandex.ru>
2026-06-09 22:11:33 +0500

    added sessions migration

--- client/src/stores/user.ts
+++ client/src/stores/user.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import Cookies from 'js-cookie'
 
-const API_URL = 'http://127.0.0.1:8000/api'
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
 export const useUserStore = defineStore('user', () => {
   const user = ref<any>(null)
